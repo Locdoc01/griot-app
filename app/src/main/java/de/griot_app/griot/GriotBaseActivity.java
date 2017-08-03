1809a369777a60241777b2af34fb6cc1df48a3b0
Marcel Schoob <marcel.schoob@live.de>
2017-08-04 01:13:01 +0200

    app-bar added / basic inheritance hierarchy of GriotBaseActivity and MainActivity and their layouts implemented / colors.xml edited

--- app/src/main/java/de/griot_app/griot/GriotBaseActivity.java
+++ app/src/main/java/de/griot_app/griot/GriotBaseActivity.java
@@ -2,15 +2,33 @@ package de.griot_app.griot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 /**
- *  BaseActivity for usual griot-app-activities. Provides base functionality like main menu, title bar and main button bar.
+ *  Abstract base activity for usual griot-app-activities.
+ *  Provides base functionality like main menu, title bar and main button bar.
  */
-public class GriotBaseActivity extends AppCompatActivity {
+public abstract class GriotBaseActivity extends AppCompatActivity {
+
+    private static final String TAG = GriotBaseActivity.class.getSimpleName();
+
+    private Toolbar mToolbar;
+
+    /**
+     * Abstract method, that returns the appropriate layout id for extending subclass.
+     * This method can be used in onCreate() to inflate the appropriate layout for the extending subclass
+     * @return  layout id for extending subclass
+     */
+    protected abstract int getSubClassLayoutId();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_griot_base);
+        setContentView(getSubClassLayoutId());
+
+        mToolbar = (Toolbar) findViewById(R.id.app_bar_griot_base);
+        setSupportActionBar(mToolbar);
     }
+
 }
--- app/src/main/java/de/griot_app/griot/MainActivity.java
+++ app/src/main/java/de/griot_app/griot/MainActivity.java
@@ -3,13 +3,18 @@ package de.griot_app.griot;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends GriotBaseActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        
+    }
+
+    @Override
+    protected int getSubClassLayoutId() {
+        return R.layout.activity_main;
     }
 }
