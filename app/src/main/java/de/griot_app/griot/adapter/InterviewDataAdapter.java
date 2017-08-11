a5da170e394d4207ff5bc337bc9d0f74010fb9ad
Marcel Schoob <marcel.schoob@live.de>
2017-08-11 13:50:26 +0200

    setting Image of MediaCover fixed

--- app/src/main/java/de/griot_app/griot/adapter/InterviewDataAdapter.java
+++ app/src/main/java/de/griot_app/griot/adapter/InterviewDataAdapter.java
@@ -1,6 +1,7 @@
 package de.griot_app.griot.adapter;
 
 import android.content.Context;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
@@ -79,7 +80,9 @@ public class InterviewDataAdapter extends ArrayAdapter<InterviewData> {
         holder.tvInterviewer.setText("TODO"); //TODO
         holder.tvNarrator.setText("TODO"); //TODO
         holder.tvComments.setText("TODO"); //TODO
-        holder.ivMediaCover.setImageURI(null); //TODO
+        try {
+            holder.ivMediaCover.setImageURI(Uri.parse(mListData.get(position).getPictureLocalURI())); //TODO
+        } catch(Exception e) {}
         holder.pivInterviewer.setImagePath("TODO"); //TODO
         holder.pivNarrator.setImagePath("TODO"); //TODO
 
