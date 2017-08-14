9b147686b5070d207ed09bd048dafc15a03cbc92
Marcel Schoob <marcel.schoob@live.de>
2017-08-14 12:58:31 +0200

    MainChooseFriendInputActivity implemented

--- app/src/main/java/de/griot_app/griot/mainactivities/MainChooseFriendInputActivity.java
+++ app/src/main/java/de/griot_app/griot/mainactivities/MainChooseFriendInputActivity.java
@@ -1,14 +1,41 @@
 package de.griot_app.griot.mainactivities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+
+import java.io.File;
+
+import de.griot_app.griot.ChooseTopicInputActivity;
+import de.griot_app.griot.CombinedPersonListCreator;
 import de.griot_app.griot.baseactivities.GriotBaseInputActivity;
 import de.griot_app.griot.R;
+import de.griot_app.griot.dataclasses.LocalPersonData;
+import de.griot_app.griot.dataclasses.LocalUserData;
 
 public class MainChooseFriendInputActivity extends GriotBaseInputActivity {
 
     private static final String TAG = MainChooseFriendInputActivity.class.getSimpleName();
+
+    private ListView mListViewPersons;
+    private View mListViewHeader;
+
+    private CombinedPersonListCreator mCombinedListCreator;
+
+    private Query mQueryYou;
+    private Query mQueryGuests;
+    private Query mQueryFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +46,67 @@ public class MainChooseFriendInputActivity extends GriotBaseInputActivity {
         mButtonLeft.setText(R.string.button_cancel);
         mButtonCenter.setText(R.string.button_back);
         mButtonRight.setText(R.string.button_next);
+
+        mListViewPersons = (ListView) findViewById(R.id.listView_main_input_choose_friend);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        //TODO: Auslagern, ober überarbeiten oder vereinheitlichen
+
+        mUser = mAuth.getCurrentUser();
+        mUserID = mUser.getUid();
+
+        mQueryYou = mDatabaseRootReference.child("users").orderByKey().equalTo(mUserID);
+        mQueryGuests = mDatabaseRootReference.child("guests");   //TODO genauer spezifizieren
+        mQueryFriends = mDatabaseRootReference.child("users");  //TODO genauer spezifizieren
+
+        mQueryYou.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Log.d(getSubClassTAG(), "getValueEventListener: onDataChange:");
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    mLocalUserData = ds.getValue(LocalUserData.class);
+                    mLocalUserData.setCategory(getString(R.string.text_yourself));
+                }
+
+                File file = null;
+                try {
+                    file = File.createTempFile("profile_image" + "_", ".jpg");
+                } catch (Exception e) {
+                }
+                final String path = file.getPath();
+
+                try {
+                    mStorageRef = mStorage.getReferenceFromUrl(mLocalUserData.getPictureURL());
+                    mStorageRef.getFile(file).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                            mLocalUserData.setPictureLocalURI(path);
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.e(getSubClassTAG(), "Error downloading user profile image file");
+                            mLocalUserData.setPictureLocalURI("");
+                        }
+                    });
+                } catch (Exception e) {}
+
+                mCombinedListCreator = new CombinedPersonListCreator(MainChooseFriendInputActivity.this, mLocalUserData, mListViewPersons);
+                mCombinedListCreator.add(mQueryGuests).add(mQueryFriends);
+                mCombinedListCreator.loadData();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -33,17 +121,23 @@ public class MainChooseFriendInputActivity extends GriotBaseInputActivity {
     @Override
     protected void buttonLeftPressed() {
         Log.d(TAG, "buttonLeftPressed: ");
+
+        finish();
     }
 
     @Override
     protected void buttonCenterPressed() {
         Log.d(TAG, "buttonCenterPressed: ");
+
+        finish();
     }
 
     @Override
     protected void buttonRightPressed() {
         Log.d(TAG, "buttonRightPressed: ");
-    }
 
+        startActivity(new Intent(this, ChooseTopicInputActivity.class));
+        //daten weiterreichen
+    }
 
 }
