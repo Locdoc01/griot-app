038b4713b6c21ece9f159c6add2df426f7cb4024
Marcel Schoob <marcel.schoob@live.de>
2017-08-16 02:16:44 +0200

    ChooseTopicInputActivity and TopicCatalogAdapter mostly implemented

--- app/src/main/java/de/griot_app/griot/ChooseTopicInputActivity.java
+++ app/src/main/java/de/griot_app/griot/ChooseTopicInputActivity.java
@@ -2,9 +2,13 @@ package de.griot_app.griot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.ExpandableListView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -12,7 +16,7 @@ import com.google.firebase.database.ValueEventListener;
 
 import de.griot_app.griot.adapter.TopicCatalogAdapter;
 import de.griot_app.griot.baseactivities.GriotBaseInputActivity;
-import de.griot_app.griot.dataclasses.QuestionData;
+import de.griot_app.griot.dataclasses.LocalQuestionData;
 import de.griot_app.griot.dataclasses.TopicData;
 import de.griot_app.griot.dataclasses.QuestionGroup;
 import de.griot_app.griot.dataclasses.TopicCatalog;
@@ -20,6 +24,9 @@ import de.griot_app.griot.dataclasses.TopicCatalog;
 public class ChooseTopicInputActivity extends GriotBaseInputActivity {
 
     private static final String TAG = ChooseTopicInputActivity.class.getSimpleName();
+
+    TextView mTextViewPerson;
+    ImageView mButtonCancelPerson;
 
     private String narratorID;
     private String narratorName;
@@ -49,40 +56,122 @@ public class ChooseTopicInputActivity extends GriotBaseInputActivity {
         narratorPictureURL = getIntent().getStringExtra("narratorPictureURL");
         narratorIsUser = getIntent().getBooleanExtra("narratorIsUser", true);
 
+        mTextViewPerson = (TextView) findViewById(R.id.textView_person);
+        mButtonCancelPerson = (ImageView) findViewById(R.id.button_cancel_person);
+
+        mTextViewPerson.setText(getString(R.string.text_choosed_person) + ":  " + narratorName);
+
+        mButtonCancelPerson.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
         mExpandListView = (ExpandableListView) findViewById(R.id.expandListView_input_choose_topic);
 
-        /*
-        mExpandListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+/*
+        //If a Topic-ListItem is longclicked, the topic gets selected
+        //delete the comment, if this is wanted
+        mExpandListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                if (ExpandableListView.getPackedPositionType(id)==ExpandableListView.PACKED_POSITION_TYPE_GROUP) {
+                    int groupPosition = ExpandableListView.getPackedPositionGroup(id);
+                    if (mExpandListView.isGroupExpanded(groupPosition)) {
+                        mExpandListView.collapseGroup(groupPosition);
+                        mTopicCatalog.getQuestionGroups().get(groupPosition).setExpanded(true);
+                    } else {
+                        mExpandListView.expandGroup(groupPosition);
+                        mTopicCatalog.getQuestionGroups().get(groupPosition).setExpanded(false);
+                    }
+                    /*
+                    if (mSelectedItem==null) {
+                        mSelectedItem = (QuestionGroup) mAdapter.getGroup(groupPosition);
+                        mSelectedItem.setSelected(true);
+                        mButtonRight.setEnabled(true);
+                        mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotDarkgrey, null));
+                    } else {
+                        if (mSelectedItem==mAdapter.getGroup(groupPosition)) {
+                            mSelectedItem.setSelected(false);
+                            mSelectedItem = null;
+                            mButtonRight.setEnabled(false);
+                            mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotLightgrey, null));
+                        } else {
+                            mSelectedItem.setSelected(false);
+                            mSelectedItem = (QuestionGroup) mAdapter.getGroup(groupPosition);
+                            mSelectedItem.setSelected(true);
+                            mButtonRight.setEnabled(true);
+                            mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotDarkgrey, null));
+                        }
+                    }
+                    *//*
+                    mAdapter.notifyDataSetChanged();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+*/
+
+/*
+        // If a Topic-ListItem is clicked, the topic gets selected
+        // delete the comment, if this is wanted
+        mExpandListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
                 if (mSelectedItem==null) {
-                    mSelectedItem = mAdapter.getGroup(position);
+                    mSelectedItem = (QuestionGroup) mAdapter.getGroup(groupPosition);
                     mSelectedItem.setSelected(true);
                     mButtonRight.setEnabled(true);
                     mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotDarkgrey, null));
                 } else {
-                    if (mSelectedItem==mCombinedListCreator.getAdapter().getItem(position)) {
+                    if (mSelectedItem==mAdapter.getGroup(groupPosition)) {
                         mSelectedItem.setSelected(false);
                         mSelectedItem = null;
                         mButtonRight.setEnabled(false);
                         mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotLightgrey, null));
                     } else {
                         mSelectedItem.setSelected(false);
-                        mSelectedItem = mCombinedListCreator.getAdapter().getItem(position);
+                        mSelectedItem = (QuestionGroup) mAdapter.getGroup(groupPosition);
                         mSelectedItem.setSelected(true);
                         mButtonRight.setEnabled(true);
                         mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotDarkgrey, null));
-
                     }
                 }
-                mCombinedListCreator.getAdapter().notifyDataSetChanged();
+                mAdapter.notifyDataSetChanged();
+                return true;
             }
         });
 */
 
-        mTopicCatalog = new TopicCatalog();
+        // If a Topic-ListItem is clicked, the topic gets expanded or collapsed
+        // comment out, if this is not wanted
+        mExpandListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                if (parent.isGroupExpanded(groupPosition)) {
+                    parent.collapseGroup(groupPosition);
+                    ((ImageView)((ConstraintLayout)v).findViewById(R.id.button_expand)).setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.up, null));
+                } else {
+                    parent.expandGroup(groupPosition);
+                    ((ImageView)((ConstraintLayout)v).findViewById(R.id.button_expand)).setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.down, null));
+                }
+                mAdapter.notifyDataSetChanged();
+                return true;
+            }
+        });
 
-        // TODO Daten einlesen
+        mExpandListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+
+                return false;
+            }
+        });
+
+        mTopicCatalog = new TopicCatalog();
 
         mDatabaseRef = mDatabaseRootReference.child("standardTopics");
         mDatabaseRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -103,11 +192,11 @@ public class ChooseTopicInputActivity extends GriotBaseInputActivity {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                            QuestionData questionData = ds.getValue(QuestionData.class);
+                            LocalQuestionData localQuestionData = ds.getValue(LocalQuestionData.class);
 
-                            int topicKey = questionData.getTopicKey();
-                            String question = questionData.getQuestion();
-                            mTopicCatalog.getQuestionGroups().get(topicKey).getQuestions().add(question);
+//                            int topicKey = localQuestionData.getTopicKey();
+//                            String question = localQuestionData.getQuestion();
+                            mTopicCatalog.getQuestionGroups().get(localQuestionData.getTopicKey()).getQuestions().add(localQuestionData);
                         }
 
                         //TODO: ExtraTopics und ExtraQuestions laden
@@ -130,6 +219,34 @@ public class ChooseTopicInputActivity extends GriotBaseInputActivity {
             }
         });
     }
+
+    /**
+     * Selects the topic at the specified groupPosition of the ExpandableListView. This method can be called from an OnclickListener defined in the adapter.
+     * @param groupPosition     Topic position in the ExpandableListView
+     */
+    public void buttonCheckClicked(int groupPosition) {
+        if (mSelectedItem==null) {
+            mSelectedItem = (QuestionGroup) mAdapter.getGroup(groupPosition);
+            mSelectedItem.setSelected(true);
+            mButtonRight.setEnabled(true);
+            mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotDarkgrey, null));
+        } else {
+            if (mSelectedItem==mAdapter.getGroup(groupPosition)) {
+                mSelectedItem.setSelected(false);
+                mSelectedItem = null;
+                mButtonRight.setEnabled(false);
+                mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotLightgrey, null));
+            } else {
+                mSelectedItem.setSelected(false);
+                mSelectedItem = (QuestionGroup) mAdapter.getGroup(groupPosition);
+                mSelectedItem.setSelected(true);
+                mButtonRight.setEnabled(true);
+                mButtonRight.setTextColor(ResourcesCompat.getColor(getResources(), R.color.colorGriotDarkgrey, null));
+            }
+        }
+        mAdapter.notifyDataSetChanged();
+    }
+
 
     @Override
     protected int getSubClassLayoutId() {
--- app/src/main/java/de/griot_app/griot/adapter/TopicCatalogAdapter.java
+++ app/src/main/java/de/griot_app/griot/adapter/TopicCatalogAdapter.java
@@ -1,15 +1,22 @@
 package de.griot_app.griot.adapter;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.res.ResourcesCompat;
+import android.util.Log;
 import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import de.griot_app.griot.ChooseTopicInputActivity;
 import de.griot_app.griot.R;
+import de.griot_app.griot.dataclasses.LocalPersonData;
+import de.griot_app.griot.dataclasses.LocalQuestionData;
 import de.griot_app.griot.dataclasses.QuestionGroup;
 import de.griot_app.griot.dataclasses.TopicCatalog;
 
@@ -25,6 +32,7 @@ public class TopicCatalogAdapter extends BaseExpandableListAdapter {
 
     private TextView tvTopic;
     private ImageView btnCheck;
+    private ImageView btnExpand;
     private TextView tvQuestion;
     private ImageView btnToggle;
 
@@ -33,24 +41,63 @@ public class TopicCatalogAdapter extends BaseExpandableListAdapter {
     public TopicCatalogAdapter(Context context, TopicCatalog catalog) {
         mContext = context;
         mQuestionGroups = catalog.getQuestionGroups();
+
     }
 
     @Override
-    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+    public View getGroupView(final int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.listitem_topic, null);
         }
         tvTopic = (TextView) convertView.findViewById(R.id.textView_topic);
         btnCheck = (ImageView) convertView.findViewById(R.id.button_check);
+        btnExpand = (ImageView) convertView.findViewById(R.id.button_expand);
 
         tvTopic.setText(((QuestionGroup)getGroup(groupPosition)).getTopic());
 
+        //if the button is clicked, the appropriate topic gets expanded or collaped
+        final ExpandableListView listview = (ExpandableListView) parent;
+        btnExpand.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (listview.isGroupExpanded(groupPosition)) {
+                    listview.collapseGroup(groupPosition);
+                    ((ImageView)v).setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.up, null));
+                } else {
+                    listview.expandGroup(groupPosition);
+                    ((ImageView)v).setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.down, null));
+                }
+                notifyDataSetChanged();
+            }
+        });
+
+        // if the button is clicked, the appropriate topic gets selected (or unselected)
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((ChooseTopicInputActivity)mContext).buttonCheckClicked(groupPosition);     //TODO Funktioniert nur für ChooseTopicInputActivity
+            }
+        });
+
         if (mQuestionGroups.get(groupPosition).getSelected()) {
-            btnCheck.setVisibility(View.VISIBLE);
+            btnCheck.setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.checkbox_on, null));
         } else {
-            btnCheck.setVisibility(View.GONE);
+            btnCheck.setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.checkbox_off, null));
         }
         return convertView;
+    }
+
+
+
+    @Override
+    public void onGroupCollapsed(int groupPosition) {
+        super.onGroupCollapsed(groupPosition);
+    }
+
+    @Override
+    public void onGroupExpanded(int groupPosition) {
+        super.onGroupExpanded(groupPosition);
+
     }
 
     @Override
@@ -60,7 +107,7 @@ public class TopicCatalogAdapter extends BaseExpandableListAdapter {
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
     @Override
@@ -69,15 +116,42 @@ public class TopicCatalogAdapter extends BaseExpandableListAdapter {
     }
 
     @Override
-    public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
+    public View getChildView(final int groupPosition, final int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = LayoutInflater.from(mContext).inflate(R.layout.listitem_question, null);
         }
         tvQuestion = (TextView) convertView.findViewById(R.id.textView_question);
         btnToggle = (ImageView) convertView.findViewById(R.id.button_toggle);
 
-        tvQuestion.setText((String)getChild(groupPosition, childPosition));
-        //TODO Toggle-Button setzen, überlegen, wo die Daten herkommen sollen, aus DB oder von lokalem Ort? (muss mit Themenkalatog irgendwie synchronisiert sein))
+        LocalQuestionData child = (LocalQuestionData) getChild(groupPosition, childPosition);
+        tvQuestion.setText(child.getQuestion());
+        switch (child.getQuestionState()) {
+            case LocalQuestionData.QuestionState.OFF:
+                btnToggle.setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.toggle_off, null));
+                break;
+            case LocalQuestionData.QuestionState.ON:
+                btnToggle.setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.toggle_on, null));
+                break;
+            case LocalQuestionData.QuestionState.GONE:
+                //TODO: Möglichkeit, Fragen zu löschen später implementieren
+                break;
+        }
+        //TODO überlegen, wo die QuestionStates persistent gespeichert werden sollen, in DB oder lokalem Ort? (muss mit Themenkalatog irgendwie synchronisiert sein))
+
+        btnToggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocalQuestionData data = (LocalQuestionData) getChild(groupPosition, childPosition);
+                if (data.getQuestionState()==LocalQuestionData.QuestionState.OFF) {
+                    ((ImageView)v).setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.toggle_on, null));
+                    data.setQuestionState(LocalQuestionData.QuestionState.ON);
+                } else if (data.getQuestionState()==LocalQuestionData.QuestionState.ON) {
+                    ((ImageView)v).setImageDrawable(ResourcesCompat.getDrawable(mContext.getResources(), R.drawable.toggle_off, null));
+                    data.setQuestionState(LocalQuestionData.QuestionState.OFF);
+                }
+//                notifyDataSetChanged();
+            }
+        });
         return convertView;
     }
 
