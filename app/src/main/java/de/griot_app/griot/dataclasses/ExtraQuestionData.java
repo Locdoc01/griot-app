fd54eb1ac87b8001f77f531960ef58aeabc67712
Marcel Schoob <marcel.schoob@live.de>
2017-08-20 16:11:10 +0200

    ExtraTopicData & ExtraQuestionData implemented (but have to be changed)

--- app/src/main/java/de/griot_app/griot/dataclasses/ExtraQuestionData.java
+++ app/src/main/java/de/griot_app/griot/dataclasses/ExtraQuestionData.java
@@ -1,8 +1,40 @@
 package de.griot_app.griot.dataclasses;
 
-/**
- * Created by marcel on 15.08.17.
- */
+import java.util.HashMap;
 
+/**
+ * Data holding class for extra question data
+ */
 public class ExtraQuestionData {
+
+    private HashMap<String, Boolean> topic;
+    private String question;
+
+    //default-constructor
+    public ExtraQuestionData() {
+    }
+
+    //constructor
+    public ExtraQuestionData(HashMap<String, Boolean> topic, String question) {
+        this.topic = topic;
+        this.question = question;
+    }
+
+    @Override
+    public String toString() {
+        return "ExtraQuestionData{" +
+                "topic=" + topic +
+                ", question='" + question + '\'' +
+                '}';
+    }
+
+    //get-methods
+    public HashMap<String, Boolean> getTopic() { return topic; }
+
+    public String getQuestion() { return question; }
+
+    //set-methods
+    public void setTopic(HashMap<String, Boolean> topic) { this.topic = topic; }
+
+    public void setQuestion(String question) { this.question = question; }
 }
--- app/src/main/java/de/griot_app/griot/dataclasses/ExtraTopicData.java
+++ app/src/main/java/de/griot_app/griot/dataclasses/ExtraTopicData.java
@@ -1,8 +1,31 @@
 package de.griot_app.griot.dataclasses;
 
 /**
- * Created by marcel on 15.08.17.
+ * Data holding class for extra topic data
  */
-
 public class ExtraTopicData {
+
+    private String extraTopic;
+
+    //default-constructor
+    public ExtraTopicData() {
+    }
+
+    //constructor
+    public ExtraTopicData(String extraTopic) {
+        this.extraTopic = extraTopic;
+    }
+
+    @Override
+    public String toString() {
+        return "ExtraTopicData{" +
+                "extraTopic='" + extraTopic + '\'' +
+                '}';
+    }
+
+    //get-methods
+    public String getExtraTopic() { return extraTopic; }
+
+    //set-methods
+    public void setExtraTopic(String extraTopic) { this.extraTopic = extraTopic; }
 }
